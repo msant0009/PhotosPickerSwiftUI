b7f058249d233fc6303e8daf8bbf454e32a9f8e1
Mark Santoro <mark_santoro@comcast.net>
2024-09-03 18:27:08 -0700

    Initial Commit

---
+++ PhotoPickerSwiftUI/PhotoPickerSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoPickerSwiftUIApp.swift
+//  PhotoPickerSwiftUI
+//
+//  Created by Mark Santoro on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct PhotoPickerSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
